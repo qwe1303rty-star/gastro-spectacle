365136a668709fc5e0b7386c423cb8d31f612063
Dev <dev@example.com>
2026-07-21 15:50:56 +0400

    fix: add basename to BrowserRouter for GitHub Pages subpath

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/gastro-spectacle">
       <App />
     </BrowserRouter>
   </React.StrictMode>
